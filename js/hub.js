cd9ccad8a3d32618c699712c2cfb7dc9ada07791
jannatul-akhi <jannatulferdous3608@gmail.com>
2023-11-18 00:43:11 +0600

    completed

--- js/hub.js
+++ js/hub.js
@@ -1,16 +1,23 @@
+let sorting = [];
+
 const loadData = async(dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const res = await fetch(url);
     const data = await res.json();
     displayData(data.data.tools, dataLimit);
+    sorting = data.data.tools;
 }
 
 const displayData = (hubs, dataLimit)=>{
-    // console.log(hubs);
+    console.log(hubs);
     const container = document.getElementById('container');
     toggleSpinner(true);
     container.innerHTML = '';
+
     
+    // Arr.sort(function (a, b) {
+    //     return new Date(a.date) - new Date(b.date);
+    // });
 
     const showAll = document.getElementById('see-more');
     if(dataLimit && hubs.length > 8){
@@ -20,6 +27,7 @@ const displayData = (hubs, dataLimit)=>{
     else{
         showAll.classList.add('d-none');
     }
+    
 
     hubs.forEach(hub => {
         // console.log(hub);
@@ -54,6 +62,20 @@ const displayData = (hubs, dataLimit)=>{
     toggleSpinner(false);
 }
 
+const sortByDate = () =>{
+    
+    // console.log(JSON.stringify(hubs));
+    const arr = sorting.sort(function(a,b){
+        return new Date(a.published_in) - new Date(b.published_in);
+    });
+    displayData(arr);
+}
+
+
+document.getElementById('sort-date').addEventListener('click', function(){
+    sortByDate();
+})
+
 const dataLoading = (dataLimit) =>{
     toggleSpinner(true);
     loadData(dataLimit);
@@ -81,18 +103,18 @@ const loadHubDetails = async(id) => {
 }
 
 const displayHubDetails = data => {
-    console.log(data.integrations[0]);
+    console.log(data.accuracy.score);
     const leftModal = document.getElementById('left-modal');
     leftModal.innerHTML=`
     <h5 style="color: black;" class="text-start fw-bold p-4">${data.description}</h5>
     <div class="d-flex justify-content-evenly my-4 ">
         <div style="color: green;" class="px-3 pt-4 bg-white rounded-3 fw-semibold w-25">${data.pricing[0].price ? data.pricing[0].price: 'Free of cost'}<br>/Basic</div>
         <div style="color: orange;" class="px-3 pt-4 bg-white rounded-3 fw-semibold w-25">${data.pricing[1].price ? data.pricing[1].price: 'Free of cost'}<br>/Pro</div>
-        <div style="color: red;" class="px-3 pt-4 bg-white rounded-3 fw-semibold w-25">${data.pricing[2].price ? data.pricing[2].price: 'Free of cost'}<br>/Enterprise</div>
+        <div style="color: red;" class="px-3 py-4 bg-white rounded-3 fw-semibold w-25">${data.pricing[2].price ? data.pricing[2].price: 'Free of cost'}<br>/Enterprise</div>
     </div>
 
     <div style="color: black;" class="d-flex text-start gap-2">
-        <div class="w-50 pb-5 ps-5 pe-2">
+        <div class="w-50 pb-2 ps-5 pe-2">
             <h4 class="fw-bold">Features</h4>
             <ul>
                 <li>${data.features['1'].feature_name}</li>
@@ -100,14 +122,28 @@ const displayHubDetails = data => {
                 <li>${data.features['3'].feature_name}</li>
             </ul>
         </div>
-        <div class="w-50 pb-5 ps-2 pe-5">
+        <div class="w-50 pb-2 ps-2 pe-5">
             <h4 class="fw-bold">Integrations</h4>
             <ul>
-                <li>${data.integrations[0]}</li>
-                <li>${data.integrations[1]}</li>
-                <li>${data.integrations[2]}</li>
+                <li>${data.integrations[0] ? data.integrations[0] : "No data found"}</li>
+                <li>${data.integrations[1] ? data.integrations[1] : "No data found"}</li>
+                <li>${data.integrations[2] ? data.integrations[2] : "No data found"}</li>
             </ul>
         </div>
+    </div>
+    `;
+
+    const rightModal = document.getElementById('right-modal');
+    rightModal.innerHTML = `
+    <div class="position-relative">
+        <img class="img-fluid" src="${data.image_link ? data.image_link[0] : 'No Image'}">
+        <span style="bottom:200px; right:30px;background-color:#fb5200; padding:8px 15px; color:white;border-radius:10px"  class="position-absolute translate-middle badge rounded-pill bg-danger   ${data.accuracy.score ? 'd-block' : 'd-none'}">
+     ${Math.round(data.accuracy.score * 100)} % accuacacy
+     </span>
+    </div>
+    <div class="mt-3">
+        <h4 class="fw-bold">${data.input_output_examples[0].input ? data.input_output_examples[0].input : data.input_output_examples[1].input}</h4>
+        <p>${data.input_output_examples[0].output ? data.input_output_examples[0].output : data.input_output_examples[1].output}</p>
     </div>
     `;
 
